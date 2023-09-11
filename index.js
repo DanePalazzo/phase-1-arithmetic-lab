3d0edd448adfb38f6f9f59ab048911c662affd2f
Dane Palazzo <PalazzoDD@comcast.net>
2023-09-11 15:24:45 -0600

    phase-1-arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
+const num1 = 31
+const num2 = 2
+const num3 = 8
+const num4 = 4
 
-// Write your code here
+const multiply = num1 * num2
+const random = Math.floor(Math.random() * 10) +1
+const mod = num3 - num4
+const max = Math.max(20)
+
+/*
+function multiply(){
+    return num1 * num2
+}
+
+function random(){
+    return Math.floor(Math.random() * 10) +1
+} 
+
+function mod(){
+    return num3 - num4
+}
+
+function max(){
+    return Math.max(20)
+}
+*/
+console.log(multiply())
+console.log(random())
+console.log(mod())
+console.log(max())
